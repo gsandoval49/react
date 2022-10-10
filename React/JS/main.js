60465a45285f466e02efd8b8e68b26491fd1b981
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-10-09 22:47:38 -0600

    initial commit

---
+++ React/JS/main.js
@@ -0,0 +1,14 @@
+ReactDOM.render(
+  <h1>Wello Horld</h1>,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <h1>something to say</h1>,
+  document.getElementById('root2')
+);
+
+ReactDOM.render(
+  <h2>whats going on here?</h2>,
+  document.getElementById('model1')
+);
